92c037a09297fe9868f72ef4843818f1c15a0355
Ansh Jaglan <anshjaglan2005@gmail.com>
2026-08-24 21:58:40 +0530

    add preorder

--- binarytree.cpp
+++ binarytree.cpp
@@ -31,13 +31,25 @@ Node* buildTree(vector<int> preorder){
     return root;
 }
 
+void preorderTraversal(Node* root){
+    if(root == NULL){
+        return;
+    }
+    cout << root->data << " ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
 int main(){
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     Node* root = buildTree(preorder);
-    cout << root->data << endl;
-    cout << root->left->data << endl;
-    cout << root->right->data << endl;
-    cout << root->right->left->data << endl;
+    // cout << root->data << endl;
+    // cout << root->left->data << endl;
+    // cout << root->right->data << endl;
+    // cout << root->right->left->data << endl;
+    preorderTraversal(root);
+    cout<< endl;
+    
     
     return 0;
 }
