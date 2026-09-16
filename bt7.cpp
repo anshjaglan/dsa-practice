e135c9e024873ffa1c9dfddf74051dfcd8020fad
Ansh Jaglan <anshjaglan2005@gmail.com>
2026-09-16 09:32:23 +0530

    count

---
+++ bt7.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int index = -1;
+
+Node* buildTree(vector<int> preorder){
+    index++;
+    if(preorder[index] == -1){
+        return NULL;
+    }
+    
+    Node* root = new Node(preorder[index]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+    return root;
+}
+
+//count 
+int count(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftCount = count(root->left);
+    int rightCount = count(root->right);
+    return leftCount + rightCount + 1;
+}
+
+int main(){
+    vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,6,7,8,-1,-1,-1,-1,-1};
+    Node* root = buildTree(preorder);
+    cout <<" count is : " << count(root) << endl;
+
+   return 0;
+}
