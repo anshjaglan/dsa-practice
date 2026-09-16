2eadf4c63aac37216a552f303966247ad3ef6925
Ansh Jaglan <anshjaglan2005@gmail.com>
2026-09-16 09:37:37 +0530

    sum of nodes

--- bt7.cpp
+++ bt7.cpp
@@ -38,10 +38,22 @@ int count(Node* root){
     return leftCount + rightCount + 1;
 }
 
+// sum of nodes
+
+int sumOfNodes(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftSum = sumOfNodes(root->left);
+    int rightSum = sumOfNodes(root->right);
+
+    return leftSum + rightSum + root->data;
+}
 int main(){
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,6,7,8,-1,-1,-1,-1,-1};
     Node* root = buildTree(preorder);
     cout <<" count is : " << count(root) << endl;
+    cout << "sum is the : " << sumOfNodes(root) << endl;
 
    return 0;
 }
